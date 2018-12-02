af2c5ebdc4530044e9f59da577b83855595aa101
unknown <d4rkmage80@gmail.com>
2018-12-02 11:35:38 -0500

    updated database route

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://allentan:team2871@ds123454.mlab.com:23454/todo-app-api' || 'mongodb://localhost:27017/TodoApp', {useNewUrlParser: true});
+mongoose.connect('mongodb://allentan:team2871@ds123454.mlab.com:23454/todo-app-api', {useNewUrlParser: true});
 
 module.exports.mongoose = mongoose;
